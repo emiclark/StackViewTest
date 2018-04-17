633ea44e362eae43342560c99474f7f5be0def3f
Emi Clark <emiko.clark@gmail.com>
2018-04-17 10:21:25 -0400

    added 3 labels to rec1 view-need to fix stack to expand to fit 3 labels

--- StackViewTest/StackViewTest/ViewController.swift
+++ StackViewTest/StackViewTest/ViewController.swift
@@ -12,67 +12,120 @@ class ViewController: UIViewController {
 
     var stackView1: UIStackView!
     var stackView2: UIStackView!
+    var rec1: UIView!
+    var rec2: UIView!
+    var rec3: UIView!
+    var rec4: UIView!
+    var rec5: UIView!
     
+    var label1: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.backgroundColor = .purple
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.sizeToFit()
+        return label
+    }()
+    
+    
+    var label2: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.backgroundColor = .cyan
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.sizeToFit()
+        return label
+    }()
+
+    var label3: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .darkGray
+        label.backgroundColor = .cyan
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.sizeToFit()
+        label.textAlignment = .justified
+        return label
+    }()
+    
+    // MARK:- View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
-        
-        let rec1 = UIView()
+        setupViews()
+        setupStacks()
+        setupLabels()
+    }
+    
+    // MARK:- Setup Methods
+    func setupViews() {
+        rec1 = UIView()
         rec1.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         rec1.translatesAutoresizingMaskIntoConstraints = false
         rec1.backgroundColor = .brown
         
-        let rec2 = UIView()
+        rec2 = UIView()
         rec2.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
         rec2.translatesAutoresizingMaskIntoConstraints = false
         rec2.backgroundColor = .cyan
         
-        let rec3 = UIView()
+        rec3 = UIView()
         rec3.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
         rec3.translatesAutoresizingMaskIntoConstraints = false
         rec3.backgroundColor = .green
-
-        let rec4 = UIView()
+        
+        rec4 = UIView()
         rec4.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         rec4.translatesAutoresizingMaskIntoConstraints = false
         rec4.backgroundColor = UIColor.blue
         
-        let rec5 = UIView()
+        rec5 = UIView()
         rec5.frame = CGRect(x: 0, y: 0, width: 250, height: 150)
         rec5.translatesAutoresizingMaskIntoConstraints = false
         rec5.backgroundColor = UIColor.red
-        
-        
+    }
+    
+    func setupStacks() {
         // create stackViews
         stackView1 = UIStackView()
         stackView2 = UIStackView()
-
+        
         // allow autolayout
         stackView1.translatesAutoresizingMaskIntoConstraints = false
         stackView2.translatesAutoresizingMaskIntoConstraints = false
-
+        
         // add views to stack1
         stackView1.addArrangedSubview(rec1)
         stackView1.addArrangedSubview(rec2)
         stackView1.addArrangedSubview(rec3)
-
+        
         // add views to stackview2
         stackView2.addArrangedSubview(rec4)
         stackView2.addArrangedSubview(rec5)
-  
+        
         // add stackViews to main view
         view.addSubview(stackView1)
         view.addSubview(stackView2)
-
+        
         // stackview1: set parameters
+        
+        // FIXME: set stackView1 parameter to allow stack to expand as content grows
+
         stackView1.axis = .vertical
         stackView1.spacing = 5
-        stackView1.distribution = .fillEqually
+        stackView1.distribution = .fillProportionally
         stackView1.alignment = .fill
         
         // stackview1: set constraints
         stackView1.anchor(top: view.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: nil, padding: .init(top: 80, left: 16, bottom: 16, right: 16))
-        stackView1.anchorSize(height: 250, width: 0)
         
         // set parameters for stack view 2
         stackView2.axis = .horizontal
@@ -83,10 +136,34 @@ class ViewController: UIViewController {
         // stackview2: set constraints
         stackView2.anchor(top: stackView1.bottomAnchor, leading: stackView1.leadingAnchor, trailing: stackView1.trailingAnchor, bottom: view.bottomAnchor, padding: .init(top: 20, left: 0, bottom: 20, right: 0))
         
-        stackView2.anchorSize(height: 200, width: 0)
+    }
+    
+    func setupLabels() {
+        // create labels
+        let label1 = self.label1
+        let label2 = self.label2
+        let label3 = self.label3
+
+        // set label content
+        label1.text = "this is a single line"
+        label2.text = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+        label3.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu"
+
+        // add to view
+        rec1.addSubview(label1)
+        rec1.addSubview(label2)
+        rec1.addSubview(label3)
+
+        // constrain labels
+        label1.anchor(top: rec1.topAnchor, leading: rec1.leadingAnchor, trailing: rec1.trailingAnchor, bottom: nil, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
+        
+        label2.anchor(top: label1.bottomAnchor, leading: rec1.leadingAnchor, trailing: rec1.trailingAnchor, bottom: nil, padding: .init(top: 10, left: 10, bottom: 0, right: 10))
+        
+        label3.anchor(top: label2.bottomAnchor, leading: rec2.leadingAnchor, trailing: rec2.trailingAnchor, bottom: nil, padding: .init(top: 10, left: 15, bottom: 0, right: 15))
     }
 }
 
+// MARK:- Extension
 extension UIView {
     
     func anchorSize(height: CGFloat, width: CGFloat) {
